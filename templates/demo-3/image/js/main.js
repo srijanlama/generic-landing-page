478e481e84550dfa23debea054597037e764a9f1
srijan <srijanlama81@gmail.com>
2021-12-07 13:10:16 +0545

    initial upload

---
+++ templates/demo-3/image/js/main.js
@@ -0,0 +1,150 @@
+/*
+	Appdent Theme Scripts
+*/
+
+(function($){ "use strict";
+             
+    $(window).on('load', function() {
+        $('body').addClass('loaded');
+    });
+
+/*=========================================================================
+	Sticky Header
+=========================================================================*/ 
+	$(function() {
+		var header = $("#header"),
+			yOffset = 0,
+			triggerPoint = 80;
+		$(window).on( 'scroll', function() {
+			yOffset = $(window).scrollTop();
+
+			if (yOffset >= triggerPoint) {
+				header.addClass("navbar-fixed");
+			} else {
+				header.removeClass("navbar-fixed");
+			}
+
+		});
+	});
+             
+/*=========================================================================
+   App Carousel
+=========================================================================*/
+    $('#app_carousel').owlCarousel({
+        loop:true,
+        margin: 20,
+        autoplay: true,
+        responsiveClass:true,
+        smartSpeed: 500,
+        dots: false,
+        nav:true,
+        navText: ['<i class=" arrow_carrot-left"></i>', '<i class=" arrow_carrot-right"></i>'],
+        responsive:{
+            0:{
+                items:1
+            },
+            400:{
+                items:2
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:4
+            }
+        }
+        
+    });
+
+/*=========================================================================
+	Testimonial Carousel
+=========================================================================*/
+    $('#testimonial_carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        smartSpeed: 500,
+        items: 1,
+        nav: false
+    });
+			 
+/*=========================================================================
+	Counter Up Active
+=========================================================================*/
+	var counterSelector = $('.counter');
+	counterSelector.counterUp({
+		delay: 10,
+		time: 1000
+	});
+			 
+/*=========================================================================
+	Initialize smoothscroll plugin
+=========================================================================*/
+	smoothScroll.init({
+		offset: 60
+	});
+
+/*=========================================================================
+	Active venobox
+=========================================================================*/
+	var vbSelector = $('.img_popup');
+	vbSelector.venobox({
+		numeratio: true,
+		infinigall: true
+	}); 
+				 
+/*=========================================================================
+	Scroll To Top
+=========================================================================*/ 
+    $(window).on( 'scroll', function () {
+        if ($(this).scrollTop() > 100) {
+            $('#scroll-to-top').fadeIn();
+        } else {
+            $('#scroll-to-top').fadeOut();
+        }
+    });
+             
+
+/*=========================================================================
+    wow Settings
+=========================================================================*/
+    var wow = new WOW( {
+        mobile: false,
+        offset: 150
+    });
+    wow.init();
+	
+/*=========================================================================
+	MAILCHIMP
+=========================================================================*/ 
+
+    if ($('.subscribe_form').length>0) {
+        /*  MAILCHIMP  */
+        $('.subscribe_form').ajaxChimp({
+            language: 'es',
+            callback: mailchimpCallback,
+            url: "//alexatheme.us14.list-manage.com/subscribe/post?u=48e55a88ece7641124b31a029&amp;id=361ec5b369" 
+        });
+    }
+
+    function mailchimpCallback(resp) {
+        if (resp.result === 'success') {
+            $('#subscribe-result').addClass('subs-result');
+            $('.subscription-success').text(resp.msg).fadeIn();
+            $('.subscription-error').fadeOut();
+
+        } else if(resp.result === 'error') {
+            $('#subscribe-result').addClass('subs-result');
+            $('.subscription-error').text(resp.msg).fadeIn();
+        }
+    }
+    $.ajaxChimp.translations.es = {
+        'submit': 'Submitting...',
+        0: 'We have sent you a confirmation email',
+        1: 'Please enter your email',
+        2: 'An email address must contain a single @',
+        3: 'The domain portion of the email address is invalid (the portion after the @: )',
+        4: 'The username portion of the email address is invalid (the portion before the @: )',
+        5: 'This email address looks fake or invalid. Please enter a real email address'
+    };
+
+})(jQuery);
